dc72a3549e81c2fba8acb71593b6e7b01f46c7a6
Lazaros Georgiadis <paoki200@gmail.com>
2024-02-27 23:03:27 +0200

    Working not clean

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=16951aa8652f9d1a5409edfc010d65b1`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=16951aa8652f9d1a5409edfc010d65b1`;
 
   const checkKeyPressed = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" || e.type === "click") {
       axios.get(url).then((response) => {
         setData(response.data);
         console.log(response.data);
@@ -23,36 +23,49 @@ function App() {
 
   return (
     <div
-      className="container min-vw-100 vh-100 bg-dark text-light d-flex justify-content-center"
+      className="container min-vw-100 min-vh-100 bg-dark text-light d-flex justify-content-center flex-column align-items-center"
       style={{ fontFamily: "Outfit" }}
     >
-      <div className="search">
+      <div className="search d-flex flex-row mt-3 mb-3 w-25">
         <input
-          className="m-2 rounded-3"
+          className="m-2 rounded-3 w-100"
           ref={inputRef}
           type="text"
           placeholder="Search"
           onKeyUp={checkKeyPressed}
           onChange={(e) => setLocation(e.target.value)}
         />
-        <button className="m-2 rounded-3" onClick={checkKeyPressed}>
+        <button className="m-2 rounded-3 w-25 h-50" onClick={checkKeyPressed}>
           Search
         </button>
       </div>
       <div
-        className="inner-container w-100  d-flex align-items-center flex-column justify-content-between mt-5 "
+        className="inner-container w-100 flex-grow-1 d-flex align-items-center flex-column justify-content-between"
         style={{ minWidth: "565px", maxWidth: "65%" }}
       >
         <div className="top w-100 d-flex justify-content-between">
           <div className="location">
-            <h1>{data.name}</h1>
+            <h1>
+              {data.name ? (
+                data.name
+              ) : (
+                <span
+                  style={{
+                    marginLeft: "4rem",
+                    fontSize: "65px",
+                  }}
+                >
+                  Good to see you.
+                </span>
+              )}
+            </h1>
             <div className="tempContainer d-flex flex-row">
               {data.main ? (
                 <>
                   <p className="mb-2 me-5 fs-3">Today:</p>
                   <span className="temp fw-bold" style={{ fontSize: "150px" }}>
                     {""}
-                    {(((data.main.temp - 32) * 5) / 9).toFixed(1)}
+                    {data.main.temp.toFixed(1)}
                     {" °C"}
                   </span>
                 </>
@@ -71,9 +84,18 @@ function App() {
           className="bottom d-flex justify-content-evenly align-items-center w-100 fs-3 rounded-4 mb-3"
           style={{ backgroundColor: "rgba(255,255,255,0.15)" }}
         >
-          <BottomReusable value="20 C" label="Feels Like" />
-          <BottomReusable value="20 %" label="Humidity" />
-          <BottomReusable value="15 km/h" label="Wind Speed" />
+          <BottomReusable
+            value={data.main ? `${data.main.feels_like.toFixed(1)} °C` : "-"}
+            label="Feels Like"
+          />
+          <BottomReusable
+            value={data.main ? `${data.main.humidity} %` : "-"}
+            label="Humidity"
+          />
+          <BottomReusable
+            value={data.wind ? `${data.wind.speed} km/h` : "-"}
+            label="Wind Speed"
+          />
         </div>
       </div>
     </div>
